f53f9fc01cada18882d0f6811a0cbcd3657cc6b2
Mattie Nash <mnash@kittymail.com>
2022-05-11 07:55:00 -0500

    Copied from repo

---
+++ src/main/java/frcConfigLib/libraries/controls.java
@@ -0,0 +1,61 @@
+package frcConfigLib.libraries;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import org.json.*;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.Joystick;
+public class controls {
+    static HashMap<String, Object[]> controls = new HashMap<>();
+    static Joystick joystick;
+    public controls(String controlsConfigName){
+        JSONObject controlsConfig;
+        Path controlsConfigPath = Filesystem.getDeployDirectory().toPath().resolve(controlsConfigName);
+        try{
+            controlsConfig = new JSONObject(Files.readString(controlsConfigPath));
+
+            JSONArray controllerList = controlsConfig.getJSONArray("controllers");
+            HashMap<String, Joystick> controllers = new HashMap<>();
+            for (int controllerNum = 0; controllerNum < controllerList.length(); controllerNum++) {
+                JSONObject controller = controllerList.getJSONObject(controllerNum);
+                controllers.put(controller.getString("name"), new Joystick(controller.getInt("controllerPort")));
+                joystick = new Joystick(controller.getInt("controllerPort"));
+            }
+            
+            JSONArray controlList = controlsConfig.getJSONArray("controls");
+            for (int controlNum = 0; controlNum < controlList.length(); controlNum++) {
+                JSONObject control = controlList.getJSONObject(controlNum);
+                String controlName = control.getString("controlName");
+                String controlType = control.getString("controlType");
+
+                int inputNumber = control.getInt("inputID");
+                String controllerName = control.getString("controllerName");
+                Object[] inputInfo = {controllers.get(controllerName), inputNumber, controlType};
+                controls.put(controlName, inputInfo);
+
+            }
+        }catch(IOException e){
+            System.exit(1);
+        }
+    }
+    public Object getInput(String inputName){
+        if(inputName == "controller"){
+            return joystick;
+        }
+        Object[] input = controls.get(inputName);
+        if(input == null){
+            return 0;
+        }
+        Joystick controller = (Joystick) input[0];
+        if(input[2].equals("axis")){
+            return controller.getRawAxis((int) input[1]);
+        }else if(input[2].equals("button")){
+            return controller.getRawButton((int) input[1]);
+        }else if(input[2].equals("trigger")){
+            return controller.getRawAxis((int) input[1]) >.7;
+        }else{
+            return null;
+        }
+    }
+}
---
+++ src/main/java/frcConfigLib/libraries/devices.java
@@ -0,0 +1,49 @@
+package frcConfigLib.libraries;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import org.json.*;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+public class devices {
+    private HashMap<String, Object> devices = new HashMap<>();
+    public devices(String deviceConfigName){
+        JSONObject deviceConfig;
+        Path deviceConfigPath = Filesystem.getDeployDirectory().toPath().resolve(deviceConfigName);
+        try{
+            deviceConfig = new JSONObject(Files.readString(deviceConfigPath));
+
+            JSONArray deviceList = deviceConfig.getJSONArray("devices");
+            for (int deviceNum = 0; deviceNum < deviceList.length(); deviceNum++) {
+                JSONObject device = deviceList.getJSONObject(deviceNum);
+                String deviceName = device.getString("deviceName");
+                String deviceType = device.getString("deviceType");
+                    if(deviceType.equals("motor")){
+                        int portNumber = device.getInt("canID");
+                        boolean inverted = device.getBoolean("inverted");
+                        WPI_TalonSRX motor = new WPI_TalonSRX(portNumber);
+                        motor.setInverted(inverted);
+                        devices.put(deviceName, motor);
+                    }
+                    else if(deviceType.equals("solenoid")){
+                        int forwardPort = device.getInt("forwardPort");
+                        int reversePort = device.getInt("reversePort");
+                        DoubleSolenoid solenoid = new DoubleSolenoid(20, PneumaticsModuleType.CTREPCM, forwardPort, reversePort);
+                        devices.put(deviceName, solenoid);
+                    }
+            }
+        }catch(IOException e){
+            System.exit(1);
+        }
+    }
+
+    public Object getDevice(String deviceName){
+        return devices.get(deviceName);
+    }
+}
